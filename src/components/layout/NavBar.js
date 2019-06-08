870f7c9d3649a4aa3899bdf65ae9e826cbe1964e
Andrew Smith <codingmba@gmail.com>
2019-06-08 17:22:07 -0400

    Initial commit, pre-hooks.

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export class NavBar extends Component {
-  static defaultProps = {
-    title: 'GitHub Finder',
-    icon: 'fab fa-github'
-  };
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>{' '}
+        </li>
+        <li>
+          <Link to='/about'>About</Link>{' '}
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
+Navbar.defaultProps = {
+  title: 'GitHub Finder',
+  icon: 'fab fa-github'
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} /> 
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
-export default NavBar;
+export default Navbar;
