2abb0df36a3878a0a6ff88917f87989cf32275da
Andrew Smith <codingmba@gmail.com>
2019-06-09 07:13:21 -0400

    Adds action and reducers for Search Users.

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -22,7 +22,20 @@ const GithubState = props => {
 
   // ****ACTIONS****
   // Search Users
+  const searchUsers = async text => {
+    setLoading();
 
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    });
+  };
   // Get User
 
   // Get Repos
@@ -30,6 +43,9 @@ const GithubState = props => {
   // Clear Users
 
   // Set Loading
+  const setLoading = () => {
+    dispatch({ type: SET_LOADING });
+  };
 
   return (
     <GithubContext.Provider
@@ -37,7 +53,8 @@ const GithubState = props => {
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}
